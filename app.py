ebd9122a2aa1f03da0eb09b42f63c473de80f0c0
DJM-30 <mamanideborajael@gmail.com>
2023-04-07 15:06:37 -0300

    Se renderiza template

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Holis")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html', saludo = persona1.saludo())
+
+if __name__=='__main__':
+    from persona import Persona
+    persona1 = Persona(38655402, 'Debora Jael M')
+    app.run(debug=True)
---
+++ persona.py
@@ -0,0 +1,10 @@
+class Persona:
+    def __init__(self,dni,nombre):
+        self.dni = dni
+        self.nombre = nombre
+    
+    def __str__(self):
+        return f'Nombre: {self.nombre}'
+    
+    def saludo(self):
+        return f'Bienvenida {self.nombre}'
